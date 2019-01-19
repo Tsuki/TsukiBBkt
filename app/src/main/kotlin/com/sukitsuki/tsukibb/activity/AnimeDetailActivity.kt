7a2cc0a94ae497951fbf007f31e52c08a4f88433
Tsukatsuki Kanade <i@sukitsuki.com>
2019-01-20 03:15:20 +0800

    Add exo player

--- app/src/main/kotlin/com/sukitsuki/tsukibb/activity/AnimeDetailActivity.kt
+++ app/src/main/kotlin/com/sukitsuki/tsukibb/activity/AnimeDetailActivity.kt
@@ -2,25 +2,36 @@ package com.sukitsuki.tsukibb.activity
 
 import android.os.Bundle
 import android.util.Log
+import android.view.View
+import android.widget.ProgressBar
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
 import com.sukitsuki.tsukibb.R
 import com.sukitsuki.tsukibb.model.AnimeList
 import com.sukitsuki.tsukibb.service.TbbService
 import org.jetbrains.anko.doAsync
+import org.jetbrains.anko.uiThread
 
 class AnimeDetailActivity : AppCompatActivity() {
   private val tag: String = this.javaClass.simpleName
+  private lateinit var progressBar: ProgressBar
+  private lateinit var toolbar: Toolbar
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
     val intent = this.intent
     val animeList = intent.getParcelableExtra<AnimeList>("animeList")
-    Log.d(tag, animeList.toString())
+    setContentView(R.layout.activity_anime_detail)
+    supportActionBar
+//    toolbar = findViewById(R.id.toolbar)
+    progressBar = findViewById(R.id.playerProgressBar)
     doAsync {
       val session = TbbService.instance.fetchSeason(animeList.animeId).toFuture().get()
       Log.d(tag, session.toString())
       val pagesp = TbbService.instance.fetchPageSpecials(animeList.seasonId).toFuture().get()
       Log.d(tag, pagesp.toString())
+      uiThread {
+        progressBar.visibility = View.GONE
+      }
     }
-    setContentView(R.layout.activity_anime_detail)
   }
 }
