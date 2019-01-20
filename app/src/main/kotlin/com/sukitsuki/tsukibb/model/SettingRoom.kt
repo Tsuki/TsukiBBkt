7227120ef6cab76f25c5d4e7c5120a9870691533
Tsukatsuki Kanade <i@sukitsuki.com>
2019-01-20 15:37:06 +0800

    Add setting database

---
+++ app/src/main/kotlin/com/sukitsuki/tsukibb/model/SettingRoom.kt
@@ -0,0 +1,43 @@
+package com.sukitsuki.tsukibb.model
+
+import androidx.lifecycle.LiveData
+import androidx.room.*
+import androidx.room.RoomDatabase
+import androidx.room.Database
+
+
+@Entity(tableName = "settings", indices = [Index(value = ["key_k"], unique = true)])
+data class SettingData(
+  @PrimaryKey(autoGenerate = true) var id: Long?,
+  @ColumnInfo(name = "key_k") var key: String,
+  @ColumnInfo(name = "value_v") var value: String
+) {
+  constructor() : this(null, "", "")
+}
+
+@Dao
+interface SettingDataDao {
+
+  @Query("SELECT * from settings")
+  fun getAll(): List<SettingData>
+
+  @Query("SELECT * from settings where key_k = :key LIMIT 1")
+  fun findByKey(key: String): SettingData
+
+  @Query("SELECT * from settings where key_k = :key LIMIT 1")
+  fun findLiveByKey(key: String): LiveData<SettingData>
+
+  @Insert(onConflict = OnConflictStrategy.REPLACE)
+  fun insertSetting(settingData: SettingData)
+
+  @Update
+  fun updateSetting(vararg users: SettingData)
+
+  @Query("DELETE from settings")
+  fun deleteAll()
+}
+
+@Database(entities = [SettingData::class], version = 1)
+abstract class TbbDatabase : RoomDatabase() {
+  abstract fun settingDataDao(): SettingDataDao
+}
